aa4271dca42f11fcaa7017473b1eabe5f6deb857
Maksymilian Jachymczak <maksymilianjachymczak@gmail.com>
2023-05-07 08:02:40 +0200

    Add footer and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TopNavigationComponent
   ],
   imports: [
     BrowserModule,
